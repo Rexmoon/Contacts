e1806fc5b322f9f7e4a3880e80990a86cc1a6fbe
rexmoon <lunajose6969@gmail.com>
2024-03-06 09:18:49 -0600

    feat: Project created

---
+++ Contact/Contact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact
+//
+//  Created by Jose Luna on 3/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
